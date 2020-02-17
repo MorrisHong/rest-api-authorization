36a0b18ebc623fdba742b404abbbfdf4134ae427
gracelove <govlmo91@gmail.com>
2020-02-18 07:53:00 +0900

    inmemory방식 인증

---
+++ src/test/java/kr/gracelove/demoauthorization/DemoAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gracelove.demoauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
